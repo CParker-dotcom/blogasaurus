1f2a5f5d21581883698b5133a4a970e2d45e8d34
Chante Parker <chanteparker100@gmail.com>
2019-07-12 08:00:35 -0700

    My new blogasaurus with a button with javascript added!

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log("I'm working!!")
+<script type="text/javascript">
+  alert("This is from JavaScript");
+  </script>
+
+  const click any key to continue = document.querySelector('#Button');
+
+  Where I am from.addEventListener('click', (e) => {
+    console.log("You clicked the button");
+  document.querySelector("#response-box").style.backgroundColor = button;
+  document.querySelector("#response-box").innerHTML = "button";
+  });
